4e3ceb3b34a9e123a15d7f8797f6f7f9293bf54e
Chris Anselmo <canselmo@hybridhealth.com>
2018-04-12 09:34:00 -0700

    Added api screen, updated main view styles

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,9 +1,30 @@
 import electron from 'electron';
-import $ from 'jquery';
+import Vue from 'vue/dist/vue';
+import feather from 'feather-icons';
 
 const webFrame = electron.webFrame;
 
-$(document).ready(() => {
-  webFrame.setVisualZoomLevelLimits(1, 1);
-  webFrame.setLayoutZoomLevelLimits(0, 0);
+// Prevent zooming on mac
+webFrame.setVisualZoomLevelLimits(1, 1);
+webFrame.setLayoutZoomLevelLimits(0, 0);
+
+// Apply icons
+feather.replace();
+
+// Create Vue app
+let app = new Vue({
+  el: '#app',
+  data: {
+    imageList: [],
+    apiKey: '',
+    apiInputValue: ''
+  }
 });
+
+function addImageListItem() {
+  app.imageList.push({ fileName: 'test', status: 'done' });
+  app.imageList.push({ fileName: 'test', status: 'loading' });
+  app.imageList.push({ fileName: 'test', status: 'error' });
+}
+
+window.addI = addImageListItem;
